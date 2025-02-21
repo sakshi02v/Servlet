41469b55b14490fdd69b1a2c360b7cc56190ee5a
Sakshi Verma <sakshi02v@gmail.com>
2025-02-21 10:51:58 +0530

    [Sakshi Verma] Refactor. imported jakarta packages

--- servlets/src/main/java/org/javawebdev/helloservlet/HelloServlet.java
+++ servlets/src/main/java/org/javawebdev/helloservlet/HelloServlet.java
@@ -1,15 +1,15 @@
 package org.javawebdev.helloservlet;
 
-
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello")
+
+@WebServlet("/")
 public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
